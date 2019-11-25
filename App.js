0f79e86e825f33ea2a51d7089852e85297263bcf
Michele Morgigno <michelemorgigno@MBP-di-Michele.station>
2019-11-25 13:02:15 +0100

    Aggiunte funzionalità bluetooth alla pagina di avvio dell'app

--- App.js
+++ App.js
@@ -1,114 +1,425 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import Toast from '@remobile/react-native-toast'
+import { Buffer } from 'buffer'
+import React, { Component } from 'react'
+import { FlatList, Image, Linking, Modal, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import BluetoothSerial from 'react-native-bluetooth-serial'
 
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+global.Buffer = Buffer
+const iconv = require('iconv-lite')
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const Button = ({ title, onPress, style, textStyle }) =>
+  <TouchableOpacity style={[styles.button, style]} onPress={onPress}>
+    <Text style={[styles.buttonText, textStyle]}>{title.toUpperCase()}</Text>
+  </TouchableOpacity>
 
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
+
+const DeviceList = ({ devices, connectedId, showConnectedIcon, onDevicePress }) =>
+  <FlatList
+    style={styles.devicesList}
+    keyExtractor={(item, index) => item.id}
+    data={devices}
+    renderItem={device =>
+      <TouchableOpacity
+        underlayColor='#DDDDDD'
+        key={`${device.item.index}_${device.item.index}`}
+        style={styles.listItem}
+        onPress={() => onDevicePress(device.item)}>
+        <View style={{ flexDirection: 'row' }}>
+          {showConnectedIcon
+            ? (
+              <View style={{ width: 48, height: 48, opacity: 0.4 }}>
+                {connectedId === device.item.id
+                  ? (
+                    <Image style={{ resizeMode: 'contain', width: 24, height: 24, flex: 1 }} source={require('./images/ic_done_black_24dp.png')} />
+                  ) : null}
+              </View>
+            ) : null}
+          <View style={styles.deviceRow}>
+            <Text style={styles.deviceName}>{device.item.name}</Text>
+            <Text style={styles.deviceId}>{`<${device.item.id}>`}</Text>
           </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
+        </View>
+      </TouchableOpacity>
+    }
+  />
+
+class ControllerProject extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      isEnabled: false,
+      discovering: false,
+      devices: [],
+      unpairedDevices: [],
+      connected: false,
+      section: 0,
+      modalVisible: false
+    }
+  }
+
+  componentDidMount() {
+    this.requestEnable();
+    Promise.all([
+      BluetoothSerial.isEnabled(),
+      BluetoothSerial.list()
+    ])
+      .then((values) => {
+        const [isEnabled, devices] = values
+        this.setState({ isEnabled, devices })
+      })
+
+    BluetoothSerial.on('bluetoothEnabled', () => Toast.showShortBottom('Bluetooth attivato'))
+    BluetoothSerial.on('bluetoothDisabled', () => Toast.showShortBottom('Bluetooth disattivato'))
+    BluetoothSerial.on('error', (err) => console.log(`Error: ${err.message}`))
+    BluetoothSerial.on('connectionLost', () => {
+      if (this.state.device) {
+        Toast.showShortBottom(`La connessione al dispositivo ${this.state.device.name} si è interrotta`)
+      }
+      this.setState({ connected: false })
+    })
+  }
+
+  /**
+   * [android]
+   * request enable of bluetooth from user
+   */
+  requestEnable() {
+    BluetoothSerial.requestEnable()
+      .then((res) => this.setState({ isEnabled: true }))
+      .catch((err) => Toast.showShortBottom(err.message))
+    this.listDevices();
+  }
+
+  /**
+   * [android]
+   * enable bluetooth on device
+   */
+  enable() {
+    BluetoothSerial.enable()
+      .then((res) => this.setState({ isEnabled: true }))
+      .catch((err) => Toast.showShortBottom(err.message))
+  }
+
+  /**
+   * [android]
+   * disable bluetooth on device
+   */
+  disable() {
+    BluetoothSerial.disable()
+      .then((res) => this.setState({ isEnabled: false }))
+      .catch((err) => Toast.showShortBottom(err.message))
+  }
+
+  /**
+   * [android]
+   * toggle bluetooth
+   */
+  toggleBluetooth(value) {
+    if (value === true) {
+      this.enable()
+    } else {
+      this.disable()
+    }
+  }
+
+  /**
+   * [android]
+   * Discover unpaired devices, works only in android
+   */
+  discoverUnpaired() {
+    if (this.state.discovering) {
+      return false
+    } else {
+      this.setState({ discovering: true })
+      BluetoothSerial.discoverUnpairedDevices()
+        .then((unpairedDevices) => {
+          this.setState({ unpairedDevices, discovering: false })
+        })
+        .catch((err) => Toast.showShortBottom(err.message))
+    }
+  }
+
+  /**
+   * [android]
+   * Discover unpaired devices, works only in android
+   */
+  cancelDiscovery() {
+    if (this.state.discovering) {
+      BluetoothSerial.cancelDiscovery()
+        .then(() => {
+          this.setState({ discovering: false })
+        })
+        .catch((err) => Toast.showShortBottom(err.message))
+    }
+  }
+
+  /**
+   * [android]
+   * Pair device
+   */
+  pairDevice(device) {
+    BluetoothSerial.pairDevice(device.id)
+      .then((paired) => {
+        if (paired) {
+          Toast.showShortBottom(`Device ${device.name} paired successfully`)
+          const devices = this.state.devices
+          devices.push(device)
+          this.setState({ devices, unpairedDevices: this.state.unpairedDevices.filter((d) => d.id !== device.id) })
+        } else {
+          Toast.showShortBottom(`Device ${device.name} pairing failed`)
+        }
+      })
+      .catch((err) => Toast.showShortBottom(err.message))
+  }
+
+  /**
+   * Connect to bluetooth device by id
+   * @param  {Object} device
+   */
+  connect(device) {
+    this.setState({ connecting: true })
+    BluetoothSerial.connect(device.id)
+      .then((res) => {
+        Toast.showShortBottom(`Connected to device ${device.name}`)
+        this.setState({ device, connected: true, connecting: false })
+      })
+      .catch((err) => Toast.showShortBottom(err.message))
+  }
+
+  /**
+   * Disconnect from bluetooth device
+   */
+  disconnect() {
+    BluetoothSerial.disconnect()
+      .then(() => this.setState({ connected: false }))
+      .catch((err) => Toast.showShortBottom(err.message))
+  }
+
+  /**
+   * Toggle connection when we have active device
+   * @param  {Boolean} value
+   */
+  toggleConnect(value) {
+    if (value === true && this.state.device) {
+      this.connect(this.state.device)
+    } else {
+      this.disconnect()
+    }
+  }
+
+  /**
+   * Write message to device
+   * @param  {String} message
+   */
+  write(message) {
+    if (!this.state.connected) {
+      Toast.showShortBottom('You must connect to device first')
+    }
+
+    BluetoothSerial.write(message)
+      .then((res) => {
+        Toast.showShortBottom('Successfuly wrote to device')
+        this.setState({ connected: true })
+      })
+      .catch((err) => Toast.showShortBottom(err.message))
+  }
+
+  onDevicePress(device) {
+    this.connect(device)
+  }
+
+  writePackets(message, packetSize = 64) {
+    const toWrite = iconv.encode(message, 'cp852')
+    const writePromises = []
+    const packetCount = Math.ceil(toWrite.length / packetSize)
+
+    for (var i = 0; i < packetCount; i++) {
+      const packet = new Buffer(packetSize)
+      packet.fill(' ')
+      toWrite.copy(packet, 0, i * packetSize, (i + 1) * packetSize)
+      writePromises.push(BluetoothSerial.write(packet))
+    }
+
+    Promise.all(writePromises)
+      .then((result) => {
+      })
+  }
+
+  listDevices() {
+    Promise.all([
+      BluetoothSerial.isEnabled(),
+      BluetoothSerial.list()
+    ])
+      .then((values) => {
+        const [isEnabled, devices] = values
+        this.setState({ isEnabled, devices })
+      })
+    this.render();
+  }
+
+  setModalVisible(visible) {
+    this.setState({ modalVisible: visible });
+  }
+
+  openDevicesModal() {
+    this.setModalVisible(!this.state.modalVisible);
+  }
+
+  render() {
+    return (
+      <View style={{ flex: 1 }}>
+        <View style={styles.topBar}>
+          <Text style={styles.heading}>Controller</Text>
+          <View style={styles.enableInfoWrapper}>
+            <Image
+              source={require('./images/ic_done_black_24dp.png')}
+              style={styles.iconImage}
+              visible={this.connected}
+            />
+            <TouchableOpacity onPress={() => { this.openDevicesModal() }}>
+              <Image
+                source={require('./images/baseline_bluetooth_connected_white.png')}
+                style={styles.iconImage}
+              />
+            </TouchableOpacity>
+          </View>
+        </View>
+        <View style={{ marginTop: 22 }}>
+          <Modal
+            animationType="slide"
+            visible={this.state.modalVisible}
+            onRequestClose={() => {
+              this.setModalVisible(false);
+            }}>
+            <View>
+              <DeviceList
+                showConnectedIcon={this.state.section === 0}
+                connectedId={this.state.device && this.state.device.id}
+                devices={this.state.devices}
+                onDevicePress={(device) => this.onDevicePress(device)}
+              />
+              <Button
+                title='CHIUDI'
+                onPress={() => {
+                  this.setModalVisible(!this.state.modalVisible);
+                }}
+              />
+              <Button
+                title='DISPOSITIVO NON TROVATO?'
+                onPress={() => {
+                  Linking.openURL('');
+                }}
+              />
+            </View>
+          </Modal>
+        </View>
+      </View>
+    )
+  }
+}
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+  container: {
+    flex: 0.9,
+    backgroundColor: '#F5FCFF'
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
+  iconImage: {
+    width: 25,
+    height: 25
   },
-  body: {
-    backgroundColor: Colors.white,
+  topBar: {
+    height: 56,
+    paddingHorizontal: 16,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    elevation: 6,
+    backgroundColor: '#004c8b'
   },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  heading: {
+    fontWeight: 'bold',
+    fontSize: 16,
+    alignSelf: 'center',
+    color: '#FFFFFF',
+    width: '50%'
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
+  enableInfoWrapper: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
   },
-  sectionDescription: {
-    marginTop: 8,
+  tab: {
+    alignItems: 'center',
+    flex: 0.5,
+    height: 56,
+    justifyContent: 'center',
+    borderBottomWidth: 6,
+    borderColor: 'transparent'
+  },
+  connectionInfoWrapper: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingHorizontal: 25
+  },
+  connectionInfo: {
+    fontWeight: 'bold',
+    alignSelf: 'center',
     fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
+    marginVertical: 10,
+    color: '#238923'
   },
-  highlight: {
-    fontWeight: '700',
+  devicesList: {
+    height: '80%'
   },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
+  deviceRow: {
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+    alignItems: 'center'
   },
-});
+  deviceName: {
+    fontWeight: 'bold',
+    width: '50%'
+  },
+  deviceId: {
+    color: '#777777'
+  },
+  listItem: {
+    flex: 1,
+    height: 48,
+    paddingHorizontal: 16,
+    borderColor: '#ccc',
+    borderBottomWidth: 0.5,
+    justifyContent: 'center'
+  },
+  fixedFooter: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderTopWidth: 1,
+    borderTopColor: '#ddd'
+  },
+  button: {
+    height: '5%',
+    margin: 5,
+    paddingHorizontal: 16,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  buttonText: {
+    color: '#004c8b',
+    fontWeight: 'bold',
+    fontSize: 14,
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    textAlign: 'center'
+  },
+  buttonRaised: {
+    backgroundColor: '#7B1FA2',
+    borderRadius: 2,
+    elevation: 2
+  }
+})
 
-export default App;
+export default ControllerProject
